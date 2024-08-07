37bdf06c6d744cfe76deee6ec0f7fecad7abf905
Sebastian <sebastianvalbuena25@gmail.com>
2024-08-07 17:10:34 -0500

    subida de archivos con multer

---
+++ src/files/files.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadProductImage(@UploadedFile() file: Express.Multer.File) {
+    console.log(file)
+
+    return file;
+  }
+}
