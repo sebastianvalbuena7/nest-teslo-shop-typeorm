2621092c1553161ee102a5782046a5a131ed08f1
Sebastian <sebastianvalbuena25@gmail.com>
2024-08-08 20:33:59 -0500

    renombrar y cargar archivos

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,16 +1,29 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
+import { fileNamer } from './helpers/fileNamer.helper';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(private readonly filesService: FilesService) { }
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('file', {
+    fileFilter,
+    // limits: { fileSize: 100 }
+    storage: diskStorage({
+      destination: './static/products',
+      filename: fileNamer
+    }),
+  }))
   async uploadProductImage(@UploadedFile() file: Express.Multer.File) {
-    console.log(file)
+    if (!file)
+      throw new BadRequestException('Make sure the file is a image');
 
-    return file;
+    return {
+      fileName: file.originalname
+    }
   }
 }
